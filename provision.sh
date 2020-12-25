33e9fcb86ddca7e4bfdfb5137fa0438379155c80
Gesa Stupperich <gesa.stupperich@gmail.com>
2020-12-25 07:59:16 +0000

    No static IP setting

--- provision.sh
+++ provision.sh
@@ -11,19 +11,7 @@ sudo raspi-config nonint do_expand_rootfs
 
 # enable cgroups for k3s
 sudo raspi-config nonint do_memory_split 16
-cmdline=$(sudo cat /boot/cmdline.txt) && sudo echo -n $cmdline "cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" | sudo tee /boot/cmdline.txt
-
-# set static IP for wired connection (eth0)
-printf "interface eth0\n\n" | sudo tee -a /etc/network/interfaces
-printf "static ip_address=%s/24\n" $STATIC_IP | sudo tee -a /etc/network/interfaces
-printf "static routers=%s/24\n" $ROUTER_IP | sudo tee -a /etc/network/interfaces
-printf "static domain_name_servers=%s/24\n\n" $ROUTER_IP | sudo tee -a /etc/network/interfaces
-
-# set static IP for wireless connection (wlan0)
-printf "interface wlan0\n\n" | sudo tee -a /etc/network/interfaces
-printf "static ip_address=%s/24\n" $STATIC_IP | sudo tee -a /etc/network/interfaces
-printf "static routers=%s/24\n" $ROUTER_IP | sudo tee -a /etc/network/interfaces
-printf "static domain_name_servers=%s/24\n\n" $ROUTER_IP | sudo tee -a /etc/network/interfaces
+grep cgroup_enable=memory /boot/cmdline.txt || cmdline=$(sudo cat /boot/cmdline.txt) && sudo echo -n $cmdline "cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" | sudo tee /boot/cmdline.txt
 
 # restart to allow configuration to take full effect
-sudo shutdown -r now
+sudo reboot
--- run.sh
+++ run.sh
@@ -3,12 +3,6 @@ set -ex
 
 HOST_NAME="${HOST_NAME:raspberrypi}"
 PASSWORD="${PASSWORD:raspberry}"
-ROUTER_IP="${ROUTER_IP:10.0.0.1}"
-
-# prompt for static IP if not provided
-while [ -z $STATIC_IP ]; do
-  read -p "Please specify static IP for Raspberry Pi: " STATIC_IP
-done
 
 # copy ssh key to pi and add the host key to .ssh/known_hosts
 sed -i '' '/^raspberrypi.local/d' ~/.ssh/known_hosts
@@ -16,8 +10,8 @@ ssh-keyscan raspberrypi.local >> ~/.ssh/known_hosts
 echo raspberry | sshpass ssh-copy-id -f pi@raspberrypi.local
 
 set +e
-export HOST_NAME=$HOST_NAME PASSWORD=$PASSWORD STATIC_IP=$STATIC_IP ROUTER_IP=$ROUTER_IP
-ENV_VARS='$HOST_NAME:$PASSWORD:$STATIC_IP:$ROUTER_IP'
+export HOST_NAME=$HOST_NAME PASSWORD=$PASSWORD
+ENV_VARS='$HOST_NAME:$PASSWORD'
 envsubst "$ENV_VARS" < provision.sh | ssh pi@raspberrypi.local sh -
 #set -e
 set +x
